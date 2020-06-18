c0c2aab2619904d70fc182e8e5f391c0fe09bc53
Jerryson Guillen <jerrysonguillen@gmail.com>
2020-06-18 09:10:23 +0800

    first commit

---
+++ todo-backend/index.js
@@ -0,0 +1,54 @@
+const cors = require("cors")
+
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const PORT = process.env.PORT || 3000
+const db = require("./models/")
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+function success(res, payload) {
+  return res.status(200).json(payload)
+}
+
+app.get("/todos", async (req, res, next) => {
+  try {
+    const todos = await db.Todo.find({})
+    return success(res, todos)
+  } catch (err) {
+    next({ status: 400, message: "failed to get todos" })
+  }
+})
+
+app.post("/todos", async (req, res, next) => {
+  try {
+    const todo = await db.Todo.create(req.body)
+    return success(res, todo)
+  } catch (err) {
+    next({ status: 400, message: "failed to create todo" })
+  }
+})
+
+app.delete("/todos", async (req,res, next) => {
+  try {
+    await db.Todo.remove({})
+    const todos = await db.Todo.find({})
+    return success(res, todos)
+  } catch (err) {
+    next({ status: 400, message: "failed to clear" })
+  }
+} )
+
+app.use((err, req, res, next) => {
+  return res.status(err.status || 400).json({
+    status: err.status || 400,
+    message: err.message || "there was an error processing request",
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
